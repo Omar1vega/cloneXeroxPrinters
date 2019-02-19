9b31c4c990e85735ecf4dd00ba859ace44714fba
omar1vega <omar1vega@gmail.com>
2019-02-18 23:24:56 -0800

    applying pep8 standards

--- cloneXeroxPrinters.py
+++ cloneXeroxPrinters.py
@@ -7,80 +7,74 @@ baseCloneTuples = (('_fun_function', 'HTTP_Set_Config_Attrib_fn'),
                    ('NextPage', '/properties/cloning_dl.php'))
 
 
-def findCSRF(html):
+def find_csrf(html):
     for line in html.split('\n'):
-        if ('CSRF' in line):
+        if 'CSRF' in line:
             line = line[line.find('value=') + len('value="'):line.rfind('"')]
             return line
     return None
 
 
-def findCloneParameters(html):
-    typeOfParams = 0
-    paramList = []
-    parameterStr = ''
-    tupleList = []
+def find_clone_parameters(html):
+    parameter_str = ''
+    tuple_list = []
 
     for line in html.split('\n'):
-        if ('directoryList' in line):
-            parameterStr = line
+        if 'directoryList' in line:
+            parameter_str = line
             break
-    if (parameterStr is not ''):
+    if parameter_str is not '':
         typeOfParams = 2
-        parameterStr = parameterStr.split('=')[1].strip()
-        parameterStr = parameterStr.strip('"')
-        parameterStr = parameterStr.strip("'")
-        for entry in parameterStr.split(','):
-            if (entry.isnumeric()):
-                tupleList.append(('clone_group', entry))
+        parameter_str = parameter_str.split('=')[1].strip()
+        parameter_str = parameter_str.strip('"')
+        parameter_str = parameter_str.strip("'")
+        for entry in parameter_str.split(','):
+            if entry.isnumeric():
+                tuple_list.append(('clone_group', entry))
     else:
         typeOfParams = 1
-        ##par str never got assigned, parameter list is in checkboxes within html
+        # parameter str never got assigned, parameter list is in checkboxes within html
         for line in html.split('\n'):
-            if (('input type="checkbox"' in line) and ('[' not in line)):
+            if ('input type="checkbox"' in line) and ('[' not in line):
                 line = line.strip()
-                tupleList.append(('clone_group', line.split('"')[-2]))
-    return (tuple(tupleList), typeOfParams)
+                tuple_list.append(('clone_group', line.split('"')[-2]))
+    return tuple(tuple_list), typeOfParams
 
 
-def stripTags(htmlLine):
-    while htmlLine.find('<') != -1:
-        start = htmlLine.find('<')
-        end = htmlLine.find('>') + 1
-        htmlLine = htmlLine.replace(htmlLine[start:end], '')
-    return htmlLine
+def strip_tags(html_line):
+    while html_line.find('<') != -1:
+        start = html_line.find('<')
+        end = html_line.find('>') + 1
+        html_line = html_line.replace(html_line[start:end], '')
+    return html_line
 
 
-def getModel(printer):
+def get_model(printer):
     r = requests.get('http://' + printer + '/header.php')
     for line in r.text.split('\n'):
         if 'product' in line:
             model = line
             break
-    return stripTags(model).replace('&reg;', '')
+    return strip_tags(model).replace('&reg;', '')
 
 
-def clonePrinter(printerInfoList):
-    deptName = printerInfoList[0].strip()
-    printer = printerInfoList[1].strip()
+def clone_printer(printer_info_list):
+    dept_name = printer_info_list[0].strip()
+    printer = printer_info_list[1].strip()
     printer = printer.strip('http://')
-    user = printerInfoList[2]
-    password = printerInfoList[3]
+    user = printer_info_list[2]
+    password = printer_info_list[3]
 
-    print(
-        "***************************************************************************************************************************************")
     print("Cloning Printer:", printer)
     with requests.Session() as s:
 
-        retrievalType = 0
-
-        ##BASE SITE TO SET UP SESSION WITH COOKIES/SESSION ID
+        # BASE SITE TO SET UP SESSION WITH COOKIES/SESSION ID
         baseURL = 'http://' + printer + '/properties/cloning.php'
         print("getting base session id and cookies")
         r = s.get(baseURL)
-        csrf = findCSRF(r.text)
+        csrf = find_csrf(r.text)
 
-        ##LOGING INTO PROPERTIES PAGE
+        # LOGGING INTO PROPERTIES PAGE
         loginURL = 'http://' + printer + '/userpost/xerox.set'
         loginData = {'_fun_function': 'HTTP_Authenticate_fn', 'NextPage': '/properties/authentication/luidLogin.php',
                      'webUsername': user, 'webPassword': password, 'frmaltDomain': 'default'}
@@ -91,63 +85,61 @@ def clonePrinter(printerInfoList):
         print("logging in...")
         r = s.post(loginURL, data=loginData, cookies=requests.utils.dict_from_cookiejar(s.cookies))
 
-        ##CHEKING IF LOGIN WAS SUCCESSFUL 
+        # CHECKING IF LOGIN WAS SUCCESSFUL
         for line in r.text.split('\n'):
-            if ('invalid=t' in line):
-                print("Invalid Login Credential for", printerInfoList,
+            if 'invalid=t' in line:
+                print("Invalid Login Credential for", printer_info_list,
                       "\nUnable to access clone page\nSkipping this printer")
                 return
         print("logged in!")
 
-        ##GETTING CLONE PARAMETERS FROM BASEURL HTML (ALL THE CHECKBOXES
+        # GETTING CLONE PARAMETERS FROM BASEURL HTML (ALL THE CHECKBOXES
         r = s.get(baseURL, cookies=requests.utils.dict_from_cookiejar(s.cookies))
-        cloneParameters, retrievalType = findCloneParameters(r.text)
+        cloneParameters, retrievalType = find_clone_parameters(r.text)
         cloneParameters = baseCloneTuples + cloneParameters
         if csrf is not None:
             cloneParameters = cloneParameters + (('CSRFToken', csrf),)
 
-        ##POSTING ALL CLONE PARAMETERS FOUND ON CLONE PAGE
+        # POSTING ALL CLONE PARAMETERS FOUND ON CLONE PAGE
         clonePostURL = 'http://' + printer + '/dummypost/xerox.set'
         print("asking printer to create clone file...")
-        r = s.post(clonePostURL, data=cloneParameters, cookies=requests.utils.dict_from_cookiejar(s.cookies))
+        s.post(clonePostURL, data=cloneParameters, cookies=requests.utils.dict_from_cookiejar(s.cookies))
         print("POST COMPLETE")
 
-        if (retrievalType == 1):
+        if retrievalType == 1:
             cloneFileURL = 'http://' + printer + '/properties/cloneDownload.php'
-        elif (retrievalType == 2):
+        elif retrievalType == 2:
             cloneFileURL = 'http://' + printer + '/download/cloning.dlm'
 
-        ##DOWNLOADING CLONE FILE 
+        # DOWNLOADING CLONE FILE 
         print("downloading clone file")
         r = s.get(cloneFileURL, cookies=requests.utils.dict_from_cookiejar(s.cookies))
-        ##WRITING CLONE TO FILE
-        if not (os.path.isdir(deptName)):
-            os.makedirs(deptName)
-        fileName = deptName + '/' + datetime.datetime.today().strftime('%Y.%m.%d_') + getModel(printer).replace(' ',
-                                                                                                                '') + '_' + printer + '.dlm'
+        # WRITING CLONE TO FILE
+        if not (os.path.isdir(dept_name)):
+            os.makedirs(dept_name)
+        fileName = dept_name + '/' + datetime.datetime.today().strftime('%Y.%m.%d_') \
+                   + get_model(printer).replace(' ', '') + '_' + printer + '.dlm '
         with(open(fileName, 'wb')) as clone:
             clone.write(r.content)
             print(printer + " clone file written !")
-        print(
-            "***************************************************************************************************************************************\n\n\n")
 
 
 def main():
     if not (os.path.isfile('printers.txt')):
-        ## no config file found
-        print("""No configuration file ('printers.txt') was found in the current dirrectory
-              \nMake sure this file exists and is in the current directory with the following format seperated by commas
+        #  no config file found
+        print("""No configuration file ('printers.txt') was found in the current directory
+              \nMake sure this file exists and is in the current directory with the following format separated by commas
               \n DEPT_NAME,PRINTER_IP,USERNAME,PASSWORD""")
     else:
         with(open('printers.txt', 'r')) as file:
             lines = file.readlines()
         for line in lines:
-            if (line[0] == '#'):
+            if line[0] == '#':
                 continue
             print(line[0])
             line = line.strip('\n')
             printer = line.split(',')
-            clonePrinter(printer)
+            clone_printer(printer)
 
 
 main()
